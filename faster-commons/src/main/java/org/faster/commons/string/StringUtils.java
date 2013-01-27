e2ee5b7ede763a9a5b3ecbd7d2d4645690d92646
Wen Shangquan <faster.wen@gmail.com>
2013-01-27 09:12:17 +0800

    remove boolean relative helper methods

--- faster-commons/src/main/java/org/faster/commons/string/StringUtils.java
+++ faster-commons/src/main/java/org/faster/commons/string/StringUtils.java
@@ -395,67 +395,7 @@ public class StringUtils {
 		return sb.toString();
 	}
 
-	/**
-	 * 将布尔值转换为指定的字符串显示，而不是默认的true、false
-	 *
-	 * @param b
-	 *            需要转换的布尔值
-	 * @param trueDisplayString
-	 *            true时显示的字符串
-	 * @param falseDisplayString
-	 *            false时显示的字符串
-	 * @return 指定的字符串
-	 */
-	public static String booleanToString(boolean b, String trueDisplayString,
-			String falseDisplayString) {
-		return b ? trueDisplayString : falseDisplayString;
-	}
 
-	/**
-	 * 将指定的字符串根据真值列表转换为布尔值，考虑大小写
-	 *
-	 * @param boolString
-	 *            需要检测的字符串
-	 * @param trueDisplayStrings
-	 *            一串字符串参数，符合其中任何一个参数就转化为true
-	 * @return 布尔值
-	 */
-	public static boolean booleanFromString(String boolString,
-			String... trueDisplayStrings) {
-		for (String trueDisplayString : trueDisplayStrings) {
-			if (boolString.equals(trueDisplayString)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	/**
-	 * 将指定的字符串根据真值列表转换为布尔值，忽略大小写
-	 *
-	 * @param boolString
-	 *            需要检测的字符串
-	 * @param trueDisplayStrings
-	 *            一串字符串参数，不管大小写，符合其中任何一个参数就转化为true
-	 * @return 布尔值
-	 */
-	public static boolean booleanFromStringIgnoreCase(String boolString,
-			String... trueDisplayStrings) {
-		for (String trueDisplayString : trueDisplayStrings) {
-			if (boolString.equals(trueDisplayString)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public static String booleanToOnOffString(boolean b) {
-		return booleanToString(b, "On", "Off");
-	}
-
-	public static boolean booleanFromOnOffString(String boolString) {
-		return booleanFromStringIgnoreCase(boolString, "On");
-	}
 
 	public static int intValueOf(String intStr) {
 		if (intStr == null || intStr.length() == 0) {
