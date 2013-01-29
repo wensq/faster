f5fbb4a9b27aec6cb315b4541fe6a7dd1824b645
Wen Shangquan <faster.wen@gmail.com>
2013-01-29 18:53:31 +0800

    refactor util class name

--- faster-commons/src/test/java/org/faster/commons/BeansTest.java
+++ faster-commons/src/test/java/org/faster/commons/BeansTest.java
@@ -15,16 +15,14 @@
  */
 package org.faster.commons;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * @author sqwen
  */
-public class BeanUtilsTest {
+public class BeansTest {
 
 	@Test
 	public void testPopulate() {
