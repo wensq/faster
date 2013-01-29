85f6f157287423914376e1b544158576fecab495
Wen Shangquan <faster.wen@gmail.com>
2013-01-29 19:56:03 +0800

    refactor encrypt method

--- faster-commons/src/main/java/org/faster/commons/Encrypts.java
+++ faster-commons/src/main/java/org/faster/commons/Encrypts.java
@@ -15,10 +15,8 @@
  */
 package org.faster.commons;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+import org.apache.commons.codec.binary.Base64;
 
-import java.io.IOException;
 import java.security.MessageDigest;
 
 /**
@@ -26,42 +24,11 @@ import java.security.MessageDigest;
  *
  * @author sqwen
  */
-@SuppressWarnings("restriction")
 public class Encrypts {
 
 	private Encrypts() {}
 
-	public static final String encryptByBASE64(byte[] bytes) {
-		return new BASE64Encoder().encode(bytes);
-	}
-
-	public static final String encryptByBASE64(String plainText) {
-		if (plainText == null) {
-			return null;
-		}
-		return new BASE64Encoder().encode(plainText.getBytes());
-	}
-
-	public static final byte[] decodeByBASE64(String encryptText) {
-		if (encryptText == null) {
-			return null;
-		}
-		try {
-			return new BASE64Decoder().decodeBuffer(encryptText);
-		} catch (IOException ioe) {
-			throw new RuntimeException("Decrypt by BASE64 for:[" + encryptText + "] failed!", ioe);
-		}
-	}
-
-	public static final String decryptByBASE64(String encryptText) {
-		if (encryptText == null) {
-			return null;
-		}
-		byte[] buffer = decodeByBASE64(encryptText);
-		return new String(buffer);
-	}
-
-	public static final String encrypt(String plainText, String algorithm, String salt) {
+	public static final String encrypt(String algorithm, String plainText, String salt) {
 		MessageDigest md;
 		try {
 			md = MessageDigest.getInstance(algorithm);
@@ -73,55 +40,15 @@ public class Encrypts {
 		if (salt != null) {
 			md.update(salt.getBytes());
 		}
-		byte[] unencodedPassword = plainText.getBytes();
-		md.update(unencodedPassword);
+		byte[] unencodedText = plainText.getBytes();
+		md.update(unencodedText);
 
-		byte[] encodedPassword = md.digest();
-		return encryptByBASE64(encodedPassword);
+		byte[] encodedText = md.digest();
+        return Base64.encodeBase64URLSafeString(encodedText);
 	}
 
-	public static final String encrypt(String plainText, String algorithm) {
+	public static final String encrypt(String algorithm, String plainText) {
 		return encrypt(plainText, algorithm, null);
-	}
-
-	public static final String encryptByMD5(String plainText) {
-		return encrypt(plainText, "MD5");
-	}
-
-	public static final String encryptByMD5WithSalt(String plainText, String salt) {
-		return encrypt(plainText, "MD5", salt);
-	}
-
-	public static final String encryptBySHA(String plainText) {
-		return encrypt(plainText, "SHA");
-	}
-
-	public static final String encryptBySHAWithSalt(String plainText, String salt) {
-		return encrypt(plainText, "SHA", salt);
-	}
-
-	public static final String encryptBySHA256(String plainText) {
-		return encrypt(plainText, "SHA-256");
-	}
-
-	public static final String encryptBySHA256WithSalt(String plainText, String salt) {
-		return encrypt(plainText, "SHA-256", salt);
-	}
-
-	public static final String encryptBySHA384(String plainText) {
-		return encrypt(plainText, "SHA-384");
-	}
-
-	public static final String encryptBySHA384WithSalt(String plainText, String salt) {
-		return encrypt(plainText, "SHA-384", salt);
-	}
-
-	public static final String encryptBySHA512(String plainText) {
-		return encrypt(plainText, "SHA-512");
-	}
-
-	public static final String encryptBySHA512WithSalt(String plainText, String salt) {
-		return encrypt(plainText, "SHA-512", salt);
 	}
 
 }
--- faster-commons/src/main/java/org/faster/commons/Hashes.java
+++ faster-commons/src/main/java/org/faster/commons/Hashes.java
@@ -10,43 +10,43 @@ public class Hashes {
     private Hashes() {}
 
     public static final String md5(String plainText) {
-        return encrypt(plainText, "MD5");
+        return encrypt("MD5", plainText);
     }
 
     public static final String md5(String plainText, String salt) {
-        return encrypt(plainText, "MD5", salt);
+        return encrypt("MD5", plainText, salt);
     }
 
     public static final String sha(String plainText) {
-        return encrypt(plainText, "SHA");
+        return encrypt("SHA", plainText);
     }
 
     public static final String sha(String plainText, String salt) {
-        return encrypt(plainText, "SHA", salt);
+        return encrypt("SHA", plainText, salt);
     }
 
     public static final String sha256(String plainText) {
-        return encrypt(plainText, "SHA-256");
+        return encrypt("SHA-256", plainText);
     }
 
     public static final String sha256(String plainText, String salt) {
-        return encrypt(plainText, "SHA-256", salt);
+        return encrypt("SHA-256", plainText, salt);
     }
 
     public static final String sha384(String plainText) {
-        return encrypt(plainText, "SHA-384");
+        return encrypt("SHA-384", plainText);
     }
 
     public static final String sha384(String plainText, String salt) {
-        return encrypt(plainText, "SHA-384", salt);
+        return encrypt("SHA-384", plainText, salt);
     }
 
     public static final String sha512(String plainText) {
-        return encrypt(plainText, "SHA-512");
+        return encrypt("SHA-512", plainText);
     }
 
     public static final String sha512(String plainText, String salt) {
-        return encrypt(plainText, "SHA-512", salt);
+        return encrypt("SHA-512", plainText, salt);
     }
 
 }
