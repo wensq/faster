2918e800eea4c32b969804dd3cbdde2da8fe4a06
Wen Shangquan <faster.wen@gmail.com>
2013-09-10 09:06:46 +0800

    + add OpResult model

---
+++ faster-ws/src/main/java/org/faster/ws/OpResult.java
@@ -0,0 +1,80 @@
+package org.faster.ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author sqwen
+ */
+@XmlRootElement(name = "Result")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class OpResult {
+
+    public static final OpResult SUCCESS = new OpResult(true);
+
+    private boolean success;
+
+    private Integer code;
+
+    private String message;
+
+    public OpResult() {}
+
+    public OpResult(boolean success) {
+        this.success = success;
+    }
+
+    public OpResult(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public OpResult(boolean success, Integer code, String message) {
+        this.success = false;
+        this.code = code;
+        this.message = message;
+    }
+
+    public OpResult(Integer code, String message) {
+        this.success = false;
+        this.code = code;
+        this.message = message;
+    }
+
+    public static final OpResult failed(Integer errorCode) {
+        return new OpResult(false, errorCode, null);
+    }
+
+    public static final OpResult failed(String errorMessage) {
+        return new OpResult(false, null, errorMessage);
+    }
+
+    public static final OpResult failed(Integer errorCode, String errorMessage) {
+        return new OpResult(errorCode, errorMessage);
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
