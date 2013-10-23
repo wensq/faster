1198e3c8c571196edcbb48c1f10c2c4f03051735
Wen Shangquan <faster.wen@gmail.com>
2013-10-23 23:29:24 +0800

    + add DateXmlAdapter

---
+++ faster-util/src/main/java/org/faster/util/DateXmlAdapter.java
@@ -0,0 +1,26 @@
+package org.faster.util;
+
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.util.Date;
+
+/**
+ * @author sqwen
+ */
+public class DateXmlAdapter extends XmlAdapter<String, Date> {
+
+    private DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Override
+    public String marshal(Date v) throws Exception {
+        return v == null ? null : fmt.print(v.getTime());
+    }
+
+    @Override
+    public Date unmarshal(String v) throws Exception {
+        return v == null || v.trim().length() == 0 ? null : fmt.parseDateTime(v.trim()).toDate();
+    }
+
+}
